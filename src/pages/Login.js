46dcf0631bf4fc8381af40e7fd9479ba5b34be6d
KundanYadav12 <kundanyadav96197@gmail.com>
2025-04-02 16:09:01 +0530

    saved

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,38 +1,4 @@
-// import React, { useState } from "react";
-// import { useDispatch } from "react-redux";
-// import { useNavigate } from "react-router-dom";
-// import { login } from "../redux/authSlice";
-
-// const Login = () => {
-//   const [username, setUsername] = useState("");
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
-
-//   const handleLogin = () => {
-//     if (username.trim()) {
-//       dispatch(login({ username })); // Store user data in Redux
-//       navigate("/dashboard");
-//     }
-//   };
-
-//   return (
-//     <div className="container mt-5">
-//       <h2>Login</h2>
-//       <input
-//         type="text"
-//         placeholder="Enter your username"
-//         className="form-control"
-//         value={username}
-//         onChange={(e) => setUsername(e.target.value)}
-//       />
-//       <button className="btn btn-primary mt-3" onClick={handleLogin}>
-//         Login
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default Login;
+ 
 
 
 import React, { useState } from "react";
